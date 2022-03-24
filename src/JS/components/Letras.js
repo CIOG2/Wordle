9fb57679ce1c7d795fb9c37a6bf7f59f88132106
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-03-24 16:01:14 -0700

    todas letras listas

--- src/JS/components/Letras.js
+++ src/JS/components/Letras.js
@@ -14,13 +14,10 @@ const crearBotones = (array) => {
 
 
 const Letras = () => {
-    const buttons = [];
-
     const letrasLinea1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
     const letrasLinea2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ'];
     const letrasLinea3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-
-
+    
     const Linea1 = document.createElement('div');
     Linea1.classList.add('letras__linea');
     Linea1.append(...crearBotones(letrasLinea1));
@@ -29,14 +26,30 @@ const Letras = () => {
     Linea2.classList.add('letras__linea');
     Linea2.append(...crearBotones(letrasLinea2));
 
+
+
+    const botonDone = document.createElement('button');
+    botonDone.classList.add('letras__button--done');
+    botonDone.textContent = '✔';
+    botonDone.id = 'botonDone';
+
+    const botonBorrar = document.createElement('button');
+    botonBorrar.classList.add('letras__button--borrar');
+    botonBorrar.innerText = 'X';
+    botonBorrar.id = "botonBorrar"
+
+
+
     const Linea3 = document.createElement('div');
     Linea3.classList.add('letras__linea');
-    Linea3.append(...crearBotones(letrasLinea3));
-
+    let array3 = crearBotones(letrasLinea3);
+    array3.unshift(botonDone);
+    array3.push(botonBorrar);
+    Linea3.append(...array3);
+    
     const letterContainer = document.createElement('div');
     letterContainer.classList.add('letras__container');
     letterContainer.append( Linea1, Linea2, Linea3 );
-
 
     return letterContainer;
 }
--- src/JS/index.js
+++ src/JS/index.js
@@ -3,3 +3,30 @@ import Letras from "./components/Letras.js";
 
 const app = document.getElementById('app');
 app.append( Header(), Letras() );
+let palabra = '';
+const respuesta = "HOLA22";
+
+
+document.body.onclick = (e) => {
+    const target = e.target;
+
+    if(palabra.length < respuesta.length){    
+        if( target.classList.contains('letras__button') ) {
+            if (palabra === '') {
+                palabra = target.innerText;
+            }else{
+                palabra += target.innerText;
+            }
+            console.log("palabra: ", palabra);
+        }
+    }
+    if( target.id == 'botonBorrar' ){
+        if (palabra === '') {
+            alert("No hay nada que borrar");
+        }else{
+            let letras = palabra.length;
+            palabra = palabra.substring(0, letras-1);
+            console.log('palabra mocha: ', palabra);
+        }
+    }
+}
