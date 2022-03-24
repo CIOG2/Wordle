100d2d9b01c14092ffc60d04e9c1376e9bb261d4
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-03-24 16:24:11 -0700

    Cifrador y descifrador terminado

---
+++ src/JS/utils/CifrarTexto.js
@@ -0,0 +1,22 @@
+const CifrarTexto = () => {
+    const cifrar = (texto) => {
+        let textoCifrado = '';
+        for (let i = 0; i < texto.length; i++) {
+        textoCifrado += String.fromCharCode(texto.charCodeAt(i) + 5);
+        }
+        return textoCifrado;
+    };
+    
+    const descifrar = (texto) => {
+        let textoDescifrado = '';
+        for (let i = 0; i < texto.length; i++) {
+        textoDescifrado += String.fromCharCode(texto.charCodeAt(i) - 5);
+        }
+        return textoDescifrado;
+    };
+    
+    return {
+        cifrar,
+        descifrar,
+    };    
+}
