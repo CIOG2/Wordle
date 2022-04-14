63707031019d3be614a1239cde5d36a95ebfbd4e
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-04-13 19:24:30 -0600

    Agregue las palabras cifradas

--- src/JS/data/palabras.js
+++ src/JS/data/palabras.js
@@ -1,5 +1,12 @@
 const data = [
-    
+    'TZNOF', 'FZINT', 'WJHTW',  'SFHJW',     
+    'XJHFW', 'HJSFW', 'WFYTS',  'HFYFW',     
+    'YWZHT', 'HFRUT', '[THFQ',  'GTYJX',     
+    'IFYTX', 'ITSFX', 'KTHTX',  'WJYTX',     
+    'KTYTX', 'OZLTX', 'RFSLT',  'UFUJQ',     
+    'UFYTX', 'RFSTX', 'RFUFX',  'RTWFX',     
+    'RTYTW', 'RZSIT', 'SZGJX',  'SFIFW',     
+    'UJWWT', 'UJITX', 'XFHTX%'
 ];
 
 export default data
--- src/JS/utils/CifrarTexto.js
+++ src/JS/utils/CifrarTexto.js
@@ -19,4 +19,4 @@ const CifrarTexto = () => {
         cifrar,
         descifrar,
     };    
-}
+};
